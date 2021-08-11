8f006dafe4c51e15700ce47c7d43544122afd9a7
leonardo silva <leonardo@arbache.com>
2021-08-11 17:48:29 -0300

    licenca crm

--- tests/test_licenca_client.py
+++ tests/test_licenca_client.py
@@ -16,18 +16,3 @@ def test_retrieve_licenca(licenca_client, crm_base_url):
     response = licenca_client.licenca(codigo=codigo).get()
 
     assert response().status_code == status.HTTP_200_OK
-
-
-@responses.activate
-def test_retrieve_licenca_sem_codigo(licenca_client, crm_base_url):
-    codigo = str(uuid.uuid4())
-    responses.add(
-        method=responses.GET,
-        url=f"{crm_base_url}/backend/licencas/{codigo}",
-        status=status.HTTP_200_OK,
-        json={}
-    )
-
-    response = licenca_client.licenca(codigo=codigo).get()
-
-    assert response().status_code == status.HTTP_200_OK
