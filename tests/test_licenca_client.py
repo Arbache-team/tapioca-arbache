901bc35f3ae5b4357739a5170778000241e8b3c8
WILLAMES DE JESUS CAMPOS <61742382+Will-prog26@users.noreply.github.com>
2022-03-08 17:40:27 -0300

    [Feat] Adicionando rota de alterar data de conclusao no resource mapping de licenças (#30)

--- tests/test_licenca_client.py
+++ tests/test_licenca_client.py
@@ -16,3 +16,18 @@ def test_retrieve_licenca(licenca_client, crm_base_url):
     response = licenca_client.licenca(codigo=codigo).get()
 
     assert response().status_code == status.HTTP_200_OK
+
+
+@responses.activate
+def test_conclusao_licenca(licenca_client, crm_base_url):
+    codigo = str(uuid.uuid4())
+    responses.add(
+        method=responses.PATCH,
+        url=f"{crm_base_url}/backend/licencas/{codigo}/data-conclusao/",
+        status=status.HTTP_200_OK,
+        json={}
+    )
+
+    response = licenca_client.licenca_patch(codigo=codigo).patch()
+
+    assert response().status_code == status.HTTP_200_OK
