0eb67e674a7f20342fcdac7961b8c28d109bc50e
Willames <williames@arbache.com>
2022-10-27 15:00:22 -0300

    ajusta o codigo com o flake8

--- tests/test_relatorio_client.py
+++ tests/test_relatorio_client.py
@@ -2,11 +2,13 @@ import json
 import responses
 import status
 import pytest
-from tests.fixtures.relatorios import response_BAD_REQUEST, response_OK, request_body
 from tapioca.exceptions import ClientError
 
+
 @responses.activate
-def test_response_OK_relatorio(relatorio_client, gestao_base_url, response_OK, request_body):
+def test_response_OK_relatorio(
+    relatorio_client, gestao_base_url, response_OK, request_body
+):
     responses.add(
         method=responses.POST,
         url=f"{gestao_base_url}/backend/relatorios/",
@@ -20,7 +22,9 @@ def test_response_OK_relatorio(relatorio_client, gestao_base_url, response_OK, r
 
 
 @responses.activate
-def test_response_BAD_REQUEST(relatorio_client, gestao_base_url, response_BAD_REQUEST, request_body):
+def test_response_BAD_REQUEST(
+    relatorio_client, gestao_base_url, response_BAD_REQUEST, request_body
+):
     responses.add(
         method=responses.POST,
         url=f"{gestao_base_url}/backend/relatorios/",
@@ -33,9 +37,9 @@ def test_response_BAD_REQUEST(relatorio_client, gestao_base_url, response_BAD_RE
 
     json_erro = json.loads(str(erro.value))
     assert json_erro['status_code'] == status.HTTP_400_BAD_REQUEST
-    assert all (
-        key in response_BAD_REQUEST.keys() for key in json_erro['response_body'].keys()
+    assert all(
+        key in response_BAD_REQUEST.keys() for key in json_erro['response_body'].keys() # noqa
     )
-    assert all (
-        key in request_body.keys() for key in json_erro['request_body'].keys()
+    assert all(
+        key in request_body.keys() for key in json_erro['request_body'].keys() # noqa
     )
