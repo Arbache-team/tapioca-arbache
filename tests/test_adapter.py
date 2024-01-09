d62761a94bf6e6f8689ade37dff016fcaf0c9671
leonardo silva <leonardo@arbache.com>
2024-01-09 11:53:09 -0300

    teste de adapter sem url

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,4 +1,14 @@
+import pytest
+from tapioca_arbache.exceptions import MissingUrlExeption
+
+
 def test_adapter_especificando_url(perfil_adapter):
     api_params = {'url': 'https://teste.com.br'}
     api_root = perfil_adapter.get_api_root(api_params)
     assert api_root == api_params['url']
+
+
+def test_adapter_sem_especificar_url(perfil_adapter):
+    api_params = {}
+    with pytest.raises(MissingUrlExeption) as erro:
+        api_root = perfil_adapter.get_api_root(api_params)
